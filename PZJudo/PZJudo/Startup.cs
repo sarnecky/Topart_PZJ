a7e9bf2961d873df5fd62eb59344d952d5390980
sarnecky <ssarnecki34@gmail.com>
2016-09-24 17:51:02 +0200

    projekt startowy

---
+++ PZJudo/PZJudo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PZJudo.Startup))]
+namespace PZJudo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
