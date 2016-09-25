be9abfb8fd43b4cd39ec60313f395463822074cd
sarnecky <ssarnecki34@gmail.com>
2016-09-25 15:51:09 +0200

    eventy, pelny context i migracja do bazy

--- PZJudo/PZJudo/App_Start/Startup.Auth.cs
+++ PZJudo/PZJudo/App_Start/Startup.Auth.cs
@@ -5,6 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
+using PZJudo.DAL;
 using PZJudo.Models;
 
 namespace PZJudo
@@ -15,7 +16,7 @@ namespace PZJudo
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(MyContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
--- PZJudo/PZJudo/Controllers/HomeController.cs
+++ PZJudo/PZJudo/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PZJudo.DAL;
+using PZJudo.Models;
 
 namespace PZJudo.Controllers
 {
     public class HomeController : Controller
     {
+        MyContext db = new MyContext();
         public ActionResult Index()
-        {
+        {/*
+            var ev = new Event();
+            ev.Name = "wycieczka";
+            db.Events.Add(ev);
+            db.SaveChanges();
+            */
             return View();
         }
 
